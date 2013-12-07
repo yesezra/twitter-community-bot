ae7b2c27ce35cb90c16d72b5ad1682d68afaef99
Ezra Spier <ezra.spier@gmail.com>
2013-12-07 13:27:21 -0800

    Factor out a retweet function. Realize that Tweet IDs need to be strings.

--- server.js
+++ server.js
@@ -6,10 +6,18 @@ var twit = new Twitter({
     access_token_secret: process.env.ACCESS_TOKEN_SECRET
 });
 
-var userStream = twit.stream('user', {with: 'user', replies: 'all'});
+var userStream = twit.stream('user', { with: 'user', replies: 'all' });
 
 userStream.on('tweet', function(tweet) {
-  twit.post('statuses/retweet/:id', {id: tweet.id}, function(err, reply) {
-    console.log(err, reply);
-  });
+  console.log('received mention');
+  retweetById(tweet.id_str);
 });
+
+var retweetById = function(idStr) {
+  twit.post('statuses/retweet/:id', {id: idStr}, function(err, reply) {
+    console.log("retweeted id:" + idStr);
+    console.log(err);
+    console.log(reply);
+  });
+};
+
